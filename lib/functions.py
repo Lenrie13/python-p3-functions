637c800244dfa2ab4d17742c24f7b31cdb38de1d
Lenrie13 <lenrie.ogutu13@gmail.com>
2024-08-29 11:21:56 +0300

    update files

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,23 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
-def greet(name):
-    pass
+def greet(name = "Guido"):
+    print(f"Hello, {name}!")
 
-def greet_with_default(name="programmer"):
-    pass
+def greet_with_default(name = "programmer"):
+    print(f"Hello, {name}!")
+
+greet_with_default()
+greet_with_default("Lenrie")
 
 def add(num1, num2):
-    pass
+    num1 = 45
+    num2 = 55
+    return num1 + num2
 
 def halve(number):
-    pass
+    assert isinstance(number, (int, float))
+    return number / 2
+
